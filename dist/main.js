20067625e257d5d05ea6a46fb595d6e6e6eb5ccc
Dharani <dharani1414@gmail.com>
2024-05-02 10:34:39 -0700

    add background image

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,108 @@
-!function(){"use strict";console.log("Hello")}();
-//# sourceMappingURL=main.js.map
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scripts_example_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scripts/example.js */ \"./src/scripts/example.js\");\n\n(0,_scripts_example_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7QUFBMEM7QUFDMUNBLCtEQUFPLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2tpZHMtcXVpei1hcHAvLi9zcmMvaW5kZXguanM/YjYzNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgRXhhbXBsZSBmcm9tIFwiLi9zY3JpcHRzL2V4YW1wbGUuanNcIlxuRXhhbXBsZSgpOyJdLCJuYW1lcyI6WyJFeGFtcGxlIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ }),
+
+/***/ "./src/scripts/example.js":
+/*!********************************!*\
+  !*** ./src/scripts/example.js ***!
+  \********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": function() { return /* export default binding */ __WEBPACK_DEFAULT_EXPORT__; }\n/* harmony export */ });\n/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {\n  console.log(\"Hello\");\n}\n;//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc2NyaXB0cy9leGFtcGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFBQSw2QkFBZSxzQ0FBVTtFQUFDQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxPQUFPLENBQUM7QUFBQTtBQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8va2lkcy1xdWl6LWFwcC8uL3NyYy9zY3JpcHRzL2V4YW1wbGUuanM/ZmNlZSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbigpe2NvbnNvbGUubG9nKFwiSGVsbG9cIil9OyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/scripts/example.js\n");
+
+/***/ }),
+
+/***/ "./src/index.scss":
+/*!************************!*\
+  !*** ./src/index.scss ***!
+  \************************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nCan't find stylesheet to import.\n  ╷\n1 │ @import 'reset';\n  │         ^^^^^^^\n  ╵\n  src/index.scss 1:9  root stylesheet\n    at tryRunOrWebpackError (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/HookWebpackError.js:88:9)\n    at __webpack_require_module__ (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5241:12)\n    at __webpack_require__ (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5198:18)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5270:20\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3527:9)\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/Hook.js:18:14)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5176:43\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3482:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5138:16\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5106:15\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3527:9)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5052:8\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3490:6\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/HookWebpackError.js:68:3\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Cache.store (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:113:20)\n    at ItemCacheFacade.store (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/CacheFacade.js:141:15)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3489:11\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:99:5\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at Cache.get (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:81:18)\n    at ItemCacheFacade.get (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/CacheFacade.js:115:15)\n    at Compilation._codeGenerationModule (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3456:9)\n    at codeGen (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5040:11)\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3482:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5070:14\n    at processQueue (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/util/processAsyncTree.js:61:4)\n    at process.processTicksAndRejections (node:internal/process/task_queues:77:11)\n-- inner error --\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nCan't find stylesheet to import.\n  ╷\n1 │ @import 'reset';\n  │         ^^^^^^^\n  ╵\n  src/index.scss 1:9  root stylesheet\n    at Object.<anonymous> (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/sass-loader/dist/cjs.js!/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/src/index.scss:1:7)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:452:10\n    at Hook.eval [as call] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\n    at Hook.CALL_DELEGATE [as _call] (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/Hook.js:14:14)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5243:39\n    at tryRunOrWebpackError (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/HookWebpackError.js:83:7)\n    at __webpack_require_module__ (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5241:12)\n    at __webpack_require__ (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5198:18)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5270:20\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3527:9)\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/Hook.js:18:14)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5176:43\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3482:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5138:16\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5106:15\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3485:9)\n    at done (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3527:9)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5052:8\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3490:6\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/HookWebpackError.js:68:3\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\n    at Cache.store (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:113:20)\n    at ItemCacheFacade.store (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/CacheFacade.js:141:15)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3489:11\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:99:5\n    at Hook.eval [as callAsync] (eval at create (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\n    at Cache.get (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Cache.js:81:18)\n    at ItemCacheFacade.get (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/CacheFacade.js:115:15)\n    at Compilation._codeGenerationModule (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:3456:9)\n    at codeGen (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5040:11)\n    at symbolIterator (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3482:9)\n    at timesSync (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:2297:7)\n    at Object.eachLimit (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/neo-async/async.js:3463:5)\n    at /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/Compilation.js:5070:14\n    at processQueue (/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/webpack/lib/util/processAsyncTree.js:61:4)\n    at process.processTicksAndRejections (node:internal/process/task_queues:77:11)\n\nGenerated code for /Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/node_modules/sass-loader/dist/cjs.js!/Users/Moni/Documents/AppAcademy/Projects/kids-quiz/src/index.scss\n1 | throw new Error(\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nCan't find stylesheet to import.\\n  ╷\\n1 │ @import 'reset';\\n  │         ^^^^^^^\\n  ╵\\n  src/index.scss 1:9  root stylesheet\");");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.scss");
+/******/ 	
+/******/ })()
+;
