ec521fe33137658c26ccf97272e64635cf15fa6b
Dharani <dharani1414@gmail.com>
2024-05-03 15:47:53 -0700

    added quiz modal

---
+++ src/assests/questions.js
@@ -0,0 +1,26 @@
+export default {
+    0: {
+        text: 'What is the color of a tree?',
+        option1: 'Teal',
+        option2: 'Black',
+        option3: 'Green',
+        option4: 'Pink',
+        correctAnswer: 'option3'
+    },
+    1: {
+        text: 'How many states are there in USA?',
+        option1: '50',
+        option2: '49',
+        option3: '48',
+        option4: '47',
+        correctAnswer: 'option1'
+    },
+    2: {
+        text: 'How many legs does a spider have?',
+        option1: '7',
+        option2: '6',
+        option3: '8',
+        option4: '4',
+        correctAnswer: 'option3'
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as aboutModal from './scripts/modal.js';
+import questions from './assests/questions.js';
 
 document.addEventListener('DOMContentLoaded', ()=> {
     const openModalButton = document.getElementById('aboutButton');
@@ -7,12 +8,26 @@ document.addEventListener('DOMContentLoaded', ()=> {
     
     openModalButton.addEventListener('click', () => {
         const modal = document.getElementById('aboutModal');
-        debugger
         aboutModal.openModal(modal);
     });
     
     closeModalButton.addEventListener('click', () => {
         const modal = document.getElementById('aboutModal');
         aboutModal.closeModal(modal);
+    });
+
+    const startQuizButton = document.getElementById('startButton');
+    startQuizButton.addEventListener('click', () => {
+        debugger
+        const modal = document.getElementById('quizModal');
+        aboutModal.openModal(modal);
+    });
+
+    const nextButton = document.getElementById('nextQuestion');
+    let questionNumber = -1;
+    nextButton.addEventListener('click', () => {
+        questionNumber += 1;
+        const questionText = document.getElementById('questionText');
+        questionText.innerText = questions[questionNumber].text;
     });
 });
