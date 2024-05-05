d1dbe4476fe9e6696dcfd7c5bf1805c20db9df52
Dharani <dharani1414@gmail.com>
2024-05-05 12:38:00 -0700

    displaying random questions

--- src/assests/questions.js
+++ src/assests/questions.js
@@ -1,11 +1,11 @@
 export default {
     0: {
-        text: 'What is the color of a tree?',
-        option1: 'Teal',
-        option2: 'Black',
-        option3: 'Green',
-        option4: 'Pink',
-        correctAnswer: 'option3'
+        text: 'What is the name of a place you go to see lots of animals?',
+        option1: 'Park',
+        option2: 'Zoo',
+        option3: 'School',
+        option4: 'Sanctuary',
+        correctAnswer: 'option2'
     },
     1: {
         text: 'How many states are there in USA?',
@@ -22,5 +22,94 @@ export default {
         option3: '8',
         option4: '4',
         correctAnswer: 'option3'
+    },
+    3: {
+        text: 'If you freeze water, what do you get?',
+        option1: 'Oil',
+        option2: 'Cold Water',
+        option3: 'H2O',
+        option4: 'Ice',
+        correctAnswer: 'option4'
+    },
+    4: {
+        text: 'How many planets are in our solar system?',
+        option1: '8',
+        option2: '9',
+        option3: '7',
+        option4: '11',
+        correctAnswer: 'option1'
+    },
+    5: {
+        text: 'What do caterpillars turn into?',
+        option1: 'Worm',
+        option2: 'Grasshoper',
+        option3: 'Butterfly',
+        option4: 'Honey Bee',
+        correctAnswer: 'option3'
+    },
+    6: {
+        text: 'How many pairs of wings do bees have?',
+        option1: '3',
+        option2: '4',
+        option3: '1',
+        option4: '2',
+        correctAnswer: 'option4'
+    },
+    7: {
+        text: 'What do bees make?',
+        option1: 'Honey',
+        option2: 'Maple Syrup',
+        option3: 'Juice',
+        option4: 'Milk',
+        correctAnswer: 'option3'
+    },
+    8: {
+        text: 'How many days are in a Leap year?',
+        option1: '365',
+        option2: '366',
+        option3: '367',
+        option4: '368',
+        correctAnswer: 'option2'
+    },
+    9: {
+        text: 'Which is the fastest land animal?',
+        option1: 'Cheetah',
+        option2: 'Tiger',
+        option3: 'Leopard',
+        option4: 'Elephant',
+        correctAnswer: 'option1'
+    },
+    10: {
+        text: 'How many sides does a triangle have?',
+        option1: '4',
+        option2: '3',
+        option3: '2',
+        option4: '5',
+        correctAnswer: 'option2'
+    },
+    11: {
+        text: 'How many colors are in a rainbow?',
+        option1: '5',
+        option2: '6',
+        option3: '7',
+        option4: '8',
+        correctAnswer: 'option3'
+    },
+    12: {
+        text: 'Which is the Tallest animal in the world?',
+        option1: 'Elephant',
+        option2: 'Zebra',
+        option3: 'Ostrich',
+        option4: 'Giraffe',
+        correctAnswer: 'option4'
+    },
+    13: {
+        text: 'How many continents are there in the world?',
+        option1: 'Eight',
+        option2: 'Five',
+        option3: 'Seven',
+        option4: 'Eleven',
+        correctAnswer: 'option3'
     }
+
 }
--- src/index.js
+++ src/index.js
@@ -18,16 +18,44 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
     const startQuizButton = document.getElementById('startButton');
     startQuizButton.addEventListener('click', () => {
-        debugger
         const modal = document.getElementById('quizModal');
         aboutModal.openModal(modal);
     });
 
+    //gives total no.of questions from questions.js
+    let totalQuestions = Object.keys(questions).length; 
+    let totalQuestionsInQuiz = 5;
+    let previousQuestionNumbers = [];
+    let randomQuestionNumber = Math.floor(Math.random() * totalQuestions);
+    previousQuestionNumbers.push(randomQuestionNumber);
+    
+    const questionText = document.getElementById('questionText');
+    questionText.innerText = questions[randomQuestionNumber].text;
+    const answer1 = document.getElementById('option1');
+    const answer2 = document.getElementById('option2');
+    const answer3 = document.getElementById('option3');
+    const answer4 = document.getElementById('option4');
+    answer1.innerText = questions[randomQuestionNumber].option1;
+    answer2.innerText = questions[randomQuestionNumber].option2;
+    answer3.innerText = questions[randomQuestionNumber].option3;
+    answer4.innerText = questions[randomQuestionNumber].option4;
+
     const nextButton = document.getElementById('nextQuestion');
-    let questionNumber = -1;
     nextButton.addEventListener('click', () => {
-        questionNumber += 1;
-        const questionText = document.getElementById('questionText');
-        questionText.innerText = questions[questionNumber].text;
+        while(previousQuestionNumbers.includes(randomQuestionNumber)){
+            randomQuestionNumber = Math.floor(Math.random() * totalQuestions); 
+        }
+        previousQuestionNumbers.push(randomQuestionNumber);
+        if (previousQuestionNumbers.length === totalQuestionsInQuiz) {
+            previousQuestionNumbers = [];
+            nextButton.style.display = 'none';
+        }
+
+        questionText.innerText = questions[randomQuestionNumber].text;
+        answer1.innerText = questions[randomQuestionNumber].option1;
+        answer2.innerText = questions[randomQuestionNumber].option2;
+        answer3.innerText = questions[randomQuestionNumber].option3;
+        answer4.innerText = questions[randomQuestionNumber].option4;
+
     });
 });
